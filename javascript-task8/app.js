137807c5e0038169ee7771c483b1ad283cfd94e5
OsmanliFarid <osmanliferid686@gmail.com>
2025-03-05 14:17:14 +0400

    javascript-task8

---
+++ javascript-task8/app.js
@@ -0,0 +1,17 @@
+/*Mətni parametri kimi qəbul edən
+ və bu mətndəki herflerin sayını qaytaran numCounter funksiyasını yazın. */
+
+function numCounter(metin){
+    let say = 0
+    for(let i = 0; i < metin.length ; i++){
+       say++;
+       
+    }
+    return say;
+}
+const a = numCounter("salam");
+const b = numCounter("salam dunya");
+const c = numCounter("salam fehle");
+console.log(a)
+console.log(b)
+console.log(c)
