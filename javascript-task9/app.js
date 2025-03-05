6c696bd5005b497859d80ddb9f931f57dba1024a
OsmanliFarid <osmanliferid686@gmail.com>
2025-03-05 14:17:34 +0400

    javascript-task9

---
+++ javascript-task9/app.js
@@ -0,0 +1,15 @@
+/*Task-09
+İstənilən rəqəmin faktorialını hesablayan bir funksiya yaradın. (dövrlə və if-else ilə) */
+
+function num(){
+    let say = +prompt("reqem daxil edin");
+    let hasil = 1;
+    for(let i = 1; i <= say; i++){
+        hasil *= i;
+    }
+    return hasil;
+    
+}
+const a = num();
+console.log(a);
+
