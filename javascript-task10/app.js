3a73e859e2651d8b8aa36c4e34ef2f98c08292ca
OsmanliFarid <osmanliferid686@gmail.com>
2025-03-05 14:16:18 +0400

    javascript-task10

---
+++ javascript-task10/app.js
@@ -0,0 +1,17 @@
+/*Task-10
+İstifadəçidən rəqəm qəbul edən və onun müsbət, 
+mənfi və ya sıfır olduğunu yoxlayan bir funksiya yazın. (if-else ilə) */
+function num(){
+     let number = +prompt("reqem daxil et");
+     let mesaj = ""
+     if(number > 0){
+         mesaj =  `${number} musbet ededdir`;
+     }else if(number < 0){
+        mesaj =  `${number} menfi ededdir`;
+     }else if(number === 0){
+        mesaj =  `${number}-dir`;
+     }
+     return mesaj;
+}
+const a = num();
+console.log(a)
