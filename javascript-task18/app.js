7589ff1262ea5c897835f2fa36f063af3364e050
OsmanliFarid <osmanliferid686@gmail.com>
2025-03-05 16:52:54 +0400

    javascript-task18

---
+++ javascript-task18/app.js
@@ -0,0 +1,19 @@
+/*
+Task-18 
+isİnteger adında funksiya yazın.Bu funksiya 1 parametr alacaq və 
+parametr olaraq hər şey daxil edə bilərəm.Funksiya, əgər daxil edilən 
+parametr integerdirsə true deyilsə false qaytarsın.
+*/
+
+function inInteger(){
+    let num = +prompt("reqem daxil et");
+    let mesaj = "";
+    if(num % 1 === 0){
+       mesaj = true;
+    }else{
+        mesaj = false;
+    };
+    return mesaj;
+}
+const a = inInteger()
+console.log(a);
