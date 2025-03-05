7da58f7aa452c50cf1b1cbf3d6b3f757bc205bed
OsmanliFarid <osmanliferid686@gmail.com>
2025-03-05 16:52:32 +0400

    javascript-task14

---
+++ javascript-task14/app.js
@@ -0,0 +1,25 @@
+/*
+Task-14
+İstifadəçidən bir söz daxil etməsini tələb edən və bu sözün 
+palindrom olub olmadığını yoxlayan bir proqram yazın. (funksiya, dövr və if-else ilə) */
+
+function String(){
+    let metin = prompt("metin daxil et");
+    let mesaj = "";
+    let newMesaj = "" 
+    for(let i = metin.length - 1;i >= 0; i--){
+        newMesaj += metin[i];
+    };
+    if(metin === newMesaj){
+        
+        mesaj = newMesaj + " sozu palindromdu";
+        
+    }else{
+        mesaj = metin + " sozu palindrom deyil";
+    };
+    return mesaj;
+    
+    
+}
+const a = String()
+console.log(a)
