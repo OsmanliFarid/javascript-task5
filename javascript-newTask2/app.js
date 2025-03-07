ba6ab8289f26a908dc9a8f7ae766cb58ffb88834
OsmanliFarid <osmanliferid686@gmail.com>
2025-03-07 20:32:28 +0400

    new task 2

---
+++ javascript-newTask2/app.js
@@ -0,0 +1,18 @@
+const num = () =>{
+     let metin = prompt("mesaj daxil et")
+     let newWord2 = 0
+     let mesaj = ""
+     for(let i = 0;i < metin.length;i++){
+        let newWord = metin[i]
+        if(newWord === "0" || newWord === "1" || newWord === "2" || newWord === "3" || newWord === "4" || newWord === "5" ||
+            newWord === "6" || newWord === "7" || newWord === "8" || newWord === "9"){
+            newWord2++
+        }
+        
+        
+     }
+     return newWord2
+     
+}
+const a = num()
+console.log(a);
