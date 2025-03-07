1bfc1ef423b148df569cbf7efd98d7041c2b95d7
OsmanliFarid <osmanliferid686@gmail.com>
2025-03-07 20:33:05 +0400

    change

--- javascript-task14/app.js
+++ javascript-task14/app.js
@@ -1,25 +1,18 @@
-/*
-Task-14
-İstifadəçidən bir söz daxil etməsini tələb edən və bu sözün 
-palindrom olub olmadığını yoxlayan bir proqram yazın. (funksiya, dövr və if-else ilə) */
 
-const String = () =>{
-    let metin = prompt("metin daxil et");
-    let mesaj = "";
-    let newMesaj = "" 
-    for(let i = metin.length - 1;i >= 0; i--){
-        newMesaj += metin[i];
-    };
-    if(metin === newMesaj){
-        
-        mesaj = newMesaj + " sozu palindromdu";
-        
-    }else{
-        mesaj = metin + " sozu palindrom deyil";
-    };
-    return mesaj;
-    
-    
+
+const num = () =>{
+    let reqem = +prompt("reqem daxil et");
+    let numstr = String(reqem)
+    let degistirme = numstr.length 
+    let mesaj = ""
+    for(let i = 0; i < degistirme;i++){
+        if(degistirme === "."){
+          mesaj `${reqem} tam eded deyil`
+        }else{
+            mesaj = `${reqem} tam ededdir`
+        }
+    }
+    return mesaj
 }
-const a = String()
+const a = num()
 console.log(a)
