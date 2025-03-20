ad832c248d5620f65d3567237eb9396ae9c3832b
OsmanliFarid <osmanliferid686@gmail.com>
2025-03-20 23:31:42 +0400

    change

--- javascript-task10/app.js
+++ javascript-task10/app.js
@@ -2,16 +2,16 @@
 İstifadəçidən rəqəm qəbul edən və onun müsbət, 
 mənfi və ya sıfır olduğunu yoxlayan bir funksiya yazın. (if-else ilə) */
 const num = () =>{
-     let number = +prompt("reqem daxil et");
-     let mesaj = ""
-     if(number > 0){
-         mesaj =  `${number} musbet ededdir`;
-     }else if(number < 0){
-        mesaj =  `${number} menfi ededdir`;
-     }else if(number === 0){
-        mesaj =  `${number}-dir`;
-     }
-     return mesaj;
+   let number = +prompt("reqem daxil et");
+   let mesaj = ""
+   if(number > 0){
+       mesaj =  `${number} musbet ededdir`;
+   }else if(number < 0){
+      mesaj =  `${number} menfi ededdir`;
+   }else if(number === 0){
+      mesaj =  `${number}-dir`;
+   }
+   return mesaj;
 }
 const a = num();
 console.log(a)
